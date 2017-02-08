9ebae98c4052c4dc0062112c183c6a3bb3dbffae
Eran Goldman-Malka <erangoldman@gmail.com>
2017-02-08 12:36:00 +0200

    update readme with the submodule update

---
+++ Constants.py
@@ -0,0 +1,12 @@
+fs = 250000
+ns = 125000
+#nsamples = 125000
+fstart = 7000 
+fend = 17000
+# <ref><1024 zeros><ref><1024><ref>
+# amplitude * cos(2.*M_PI*fstart*i + 2.*M_PI*(fend-fstart)/2./(nsamples-1)*i*i + phi_start)
+nref = 1024
+fc = 20000
+p = 1
+a = 20000
+listen = 250000
--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Evologics software defined modem
 # Installation
 ```
 git clone https://github.com/marinetech/SDM.git
+cd SDM
+git submodule update --init --recursive
+cd evins
+make
 ```
 
 # Run
