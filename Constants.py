f29b14bcaa4399819b835e13a2de0bf6550ad1bc
Eran Goldman-Malka <erangoldman@gmail.com>
2017-02-20 10:43:09 +0200

    ok

--- Constants.py
+++ Constants.py
@@ -1,18 +1,32 @@
-fs = 62500 	# Sample frequency 48-72 = 250K ; 7-17 = 125K
+fs = 62500 	# Sample frequency 48-72 = 250K ; 7-17 = 62.5K
 ns = fs/10	# Number of samples in sec out of sf
 fstart = 7000 	# frequency start for the chirp / signal frequency for the sin
 fend = 17000	# frequency end for the chirp
 fc = 20000
 p = 1		# the slope of the chirp
-baseAmp = 10 	# minimal amplitude for the signal
-maxAmp = 200 	# max amplitude for the signal
-deltaAmp = 10 	# delta between each amplitude step
+baseAmp = 32000 	# minimal amplitude for the signal
+maxAmp = 10 	# max amplitude for the signal
+deltaAmp = -1000 	# delta between each amplitude step
 numberOfChirps = 1
-isSin = True 	# True = sinwave , False = Chirp
+isSin = False 	# True = sinwave , False = Chirp
+extraWait = fs * 2 # basic chirp = 1 sec ; 
 
-d4872 = {'fs':'250000','ns':'25000'}
+# 48-72 modem special settings
+d4872 = {'fs':'250000',
+	'ns':'25000',
+	'fstart':'48000',
+	'fend':'72000'
+	}
+
+# 7-17 modem special settings
+d717 = {'fs':'62500',
+	'ns':'6250',
+	'fstart':'7000',
+	'fend':'17000'
+	}
 ##########################
 # - Constants !!!! do not change under this line
 ##########################
 
 nref = 1024	# refrence number of samples
+refAmp = 32000
--- txgen.py
+++ txgen.py
@@ -3,6 +3,21 @@ import math, wave, array
 from random import randint
 import Constants as c
 
+def createREF():
+	for j in range(c.nref):
+		beta = (c.fend-c.fstart) * (math.pow((c.nref/c.fs), (-1 * c.p)))
+		i = j / c.fs
+		sample = c.refAmp * math.cos(math.pi * 2 * (beta / (1+c.p) * math.pow(i , (1 + c.p)) + c.fstart * i))
+		#print(str(int(sample)))
+		f.write(str(int(sample)))
+		f.write("\n")
+	for k in range(0,c.fs-c.ns):
+		sample = 0
+		#print(str(int(sample)))
+		f.write(str(int(sample)))
+		f.write("\n")
+
+
 def createTX(expAmp = 0):
 	print ("Generate amplitude : " + str(expAmp))
 	for m in range (0,c.numberOfChirps):
@@ -16,14 +31,20 @@ def createTX(expAmp = 0):
 			#print(str(int(sample)))
 			f.write(str(int(sample)))
 			f.write("\n")
-		for k in range(0,c.fs-c.ns):
+		for k in range(0,c.fs-c.ns + extraWait):
 			sample = 0
 			#print(str(int(sample)))
 			f.write(str(int(sample)))
 			f.write("\n")
 
 
+f = open('/tmp/ref','w')
+print("creating /tmp/ref")
+createREF()
+f.close()
 f = open('/tmp/tx','w')
+print("Creating /tmp/tx")
+createREF()
 for r in range(c.baseAmp,c.maxAmp + 1 ,c.deltaAmp):
 	createTX(r)
 f.close()
